1a0e65ef1961d4763cd61c43651285a9187b33a5
meekr <perry.lei@gmail.com>
2013-03-14 13:00:42 +0800

    Initial Commit

---
+++ YueHui/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YueHui
+//
+//  Created by Lei Perry on 3/14/13.
+//  Copyright (c) 2013 BitRice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
