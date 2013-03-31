a2e3a3719834798303885c962223be80b1078728
jimji2008 <jimji2008@Jis-MacBook-Air.local>
2013-04-01 00:34:56 +0800

    add login icon

---
+++ YueHui/Controllers/LoginViewController copy.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  YueHui
+//
+//  Created by Lei Perry on 3/14/13.
+//  Copyright (c) 2013 BitRice. All rights reserved.
+//
+
+#import "NetWorkReachability.h"
+#import "Receiver.h"
+
+@interface HomeViewController : UIViewController <NetworkReachabilityDelegate, UIAlertViewDelegate>
+
+@property (nonatomic, strong) NSString *uuid;
+@property (nonatomic, strong) NSString *token;
+
+@end
